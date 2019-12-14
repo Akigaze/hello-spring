a1eb9c4a6129f27c760eddfd7eb24e6d5bb324ce
Akigaze <1175520196@qq.com>
2019-12-14 16:42:24 +0800

    add properties example

---
+++ src/com/properties/PropertiesExample.java
@@ -0,0 +1,29 @@
+package com.properties;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Properties;
+
+public class PropertiesExample {
+  private Properties props;
+
+  public void setProps(Properties props) {
+    this.props = props;
+  }
+
+  @Override
+  public String toString() {
+    return "PropertiesExample{" +
+      "props=" + props +
+      '}';
+  }
+
+  public static void main(String[] args) {
+    String configLocation = "com/properties/propertiesApplicationContext.xml";
+    ApplicationContext context = new ClassPathXmlApplicationContext(configLocation);
+
+    PropertiesExample mysqlConn = context.getBean(PropertiesExample.class);
+    System.out.println(mysqlConn);
+  }
+}
