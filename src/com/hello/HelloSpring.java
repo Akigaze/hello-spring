77faeb315fe30f21f29b1943fe73c2fd5554e568
Akigaze <1175520196@qq.com>
2019-12-14 10:29:39 +0800

    refactor hello spring example

--- src/com/hello/HelloSpring.java
+++ src/com/hello/HelloSpring.java
@@ -1,17 +1,37 @@
 package com.hello;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class HelloSpring {
-    private String name;
+  private String name;
 
-    public String getName() {
-        return name;
-    }
+  public HelloSpring() {
+    System.out.println("use HelloSpring no args constructor");
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void sayHello(){
-        System.out.println("Hello "+this.name);
-    }
+  public void setName(String name) {
+    System.out.println("set HelloSpring name: " + name);
+    this.name = name;
+  }
+
+  private void sayHello() {
+    System.out.println("Hello " + this.name);
+  }
+
+  public static void main(String[] args) {
+    String configPath = "classpath:com/hello/applicationContext.xml";
+    ApplicationContext context = new ClassPathXmlApplicationContext(configPath);
+
+    HelloSpring spring1 = (HelloSpring) context.getBean("spring");
+    HelloSpring spring2 = context.getBean(HelloSpring.class);
+
+    spring1.sayHello();
+    spring2.sayHello();
+    System.out.println(spring1 == spring2);
+  }
 }
