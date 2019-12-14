d86665ed9fae5d435dc4a89a195abc5598ebada8
Akigaze <1175520196@qq.com>
2019-12-14 13:41:21 +0800

    refactor spring bean life cycle example

---
+++ src/com/lifecycle/Book.java
@@ -0,0 +1,85 @@
+package com.lifecycle;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Book {
+  private String name;
+  private String author;
+  private int pageSize;
+  private String status;
+
+  public Book() {
+    System.out.println("Book: no argument constructor");
+  }
+
+  public Book(String name, String author, int pageSize, String status) {
+    System.out.println("Book: all arguments constructor");
+    this.name = name;
+    this.author = author;
+    this.pageSize = pageSize;
+    this.status = status;
+  }
+
+  public void setName(String name) {
+    System.out.println("set book name: " + name);
+    this.name = name;
+  }
+
+  public void setAuthor(String author) {
+    this.author = author;
+  }
+
+  public void setPageSize(int pageSize) {
+    this.pageSize = pageSize;
+  }
+
+  public String getStatus() {
+    return this.status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+  public void onSale() {
+    System.out.println("init-method: " + this.name + " is " + this.status + " and will become selling");
+    this.status = "selling";
+  }
+
+  public void recycle() {
+    System.out.println("destroy-method: " + this.name + " is " + this.status + " and will become recycled");
+    this.status = "recycled";
+  }
+
+  @Override
+  public String toString() {
+    return "Book{" +
+      "name='" + name + '\'' +
+      ", author='" + author + '\'' +
+      ", pageSize=" + pageSize +
+      ", status='" + status + '\'' +
+      '}';
+  }
+
+  public static void main(String[] args) {
+    String configLocation = "com/lifecycle/lifecycleApplicationContext.xml";
+    ApplicationContext context = new ClassPathXmlApplicationContext(configLocation);
+
+    Book byaKuYaKo = (Book) context.getBean("byaKuYaKo");
+    System.out.println(byaKuYaKo);
+    System.out.println("\n-------------------\n");
+
+    Book houGaGou1 = (Book) context.getBean("houGaGou");
+    System.out.println(houGaGou1);
+    Book houGaGou2 = (Book) context.getBean("houGaGou");
+
+    System.out.println("\n--------- close context ----------\n");
+
+    ((ClassPathXmlApplicationContext) context).close();
+    System.out.println(byaKuYaKo);
+    System.out.println(houGaGou1);
+    System.out.println(houGaGou2);
+
+  }
+}
---
+++ src/com/lifecycle/BookPostProcessor.java
@@ -0,0 +1,27 @@
+package com.lifecycle;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class BookPostProcessor implements BeanPostProcessor {
+  //postProcessBeforeInitialization 和 postProcessAfterInitialization 可以对bean对象进行修改、替换，再返回出去
+
+  //在bean标签的init-method之前执行
+  @Override
+  public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+    Book book = (Book) bean;
+    System.out.println(beanName + " before init is " + book.getStatus() + ", will become purchased");
+    book.setStatus("purchased");
+    return bean;
+  }
+
+  //在bean标签的init-method之后执行
+  @Override
+  public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+    Book book = (Book) bean;
+    System.out.println(beanName + " after init is " + book.getStatus() + ", will become sold");
+    book.setStatus("sold");
+    return bean;
+  }
+
+}
